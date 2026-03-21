8c41d116a4173c2184c607387a4cb8d5828c4a75
Anym0ree <matvejknazev091@gmail.com>
2026-03-21 17:35:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
 import os
 
-# Токен бота (получить у @BotFather)
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-
-# Папка для хранения данных (локально, внутри контейнера)
 DATA_FOLDER = "user_data"
 
-# Создаём папку, если её нет
 if not os.path.exists(DATA_FOLDER):
     os.makedirs(DATA_FOLDER)
