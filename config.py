a1b9d5183359fa15c4d03867bb3f652f7195c003
Anym0ree <matvejknazev091@gmail.com>
2026-03-31 21:13:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 
 # Путь к папке с данными (создаётся автоматически)
-DATA_FOLDER = os.path.join(os.path.dirname(__file__), "data")  # или просто "data", если папка в корне
+
