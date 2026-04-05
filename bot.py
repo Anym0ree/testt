07d33ce60c7e1f30ead078b2a790741dee442869
Anym0ree <matvejknazev091@gmail.com>
2026-04-06 02:11:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1757,12 +1757,10 @@ async def on_shutdown_webhook(dp):
     if scheduler and scheduler.running:
         scheduler.shutdown()
 if __name__ == "__main__":
-    executor.start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        on_startup=on_startup_webhook,
-        on_shutdown=on_shutdown_webhook,
-        skip_updates=True,
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000))
+    from aiogram import executor
+    executor.start_polling(
+        dp,
+        on_startup=on_startup_polling,
+        on_shutdown=on_shutdown_polling,
+        skip_updates=True
     )
